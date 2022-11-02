f3f33e2a2cdd492844cf9090ac8bd98d079712d8
EstefaniConislla <stefani_19_94@hotmail.com>
2022-11-01 19:12:47 -0500

    adding maestros

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,7 @@ const { body } = require('express-validator');
 
 var WelcomeController = require('../controllers/welcome');
 var AlumnosController = require('../controllers/alumnos');
+let MaestrosController = require('../controllers/maestros');
 let AuthController = require('../controllers/auth');
 
 let userProtectUrl = require('../middlewares/authUser').userProtectUrl;
@@ -26,6 +27,26 @@ api.put("/alumno/:n_lista",[
 ], AlumnosController.update_alumno);
 
 api.delete("/alumno/:n_lista", AlumnosController.delete_alumno);
+
+api.get('/', WelcomeController.welcome);
+api.get('/maestros', MaestrosController.maestros);
+api.get('/maestro/:n_lista', MaestrosController.maestro);
+api.post('/maestro', userProtectUrl, [
+    body('n_cuenta').not().isEmpty(),
+    body('nombre').not().isEmpty(),
+    body('edad').not().isEmpty(),
+    body('genero').not().isEmpty(),
+    body('especialidad').not().isEmpty()
+], MaestrosController.crear_maestro); 
+
+api.put('/maestro/:n_lista', [
+    body('nombre').not().isEmpty(),
+    body('edad').not().isEmpty(),
+    body('genero').not().isEmpty(),
+    body('especialidad').not().isEmpty()
+], MaestrosController.update_maestro);
+
+api.delete('/maestro/:n_lista', MaestrosController.delete_maestro);
 
 api.post("/login",[
     body('mail').not().isEmpty(),
